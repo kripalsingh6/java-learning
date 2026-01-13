42155270a5c8eea8c2007c7b9bd60da4488eb737
kripal <thakurkripalsingh6@gmail.com>
2026-01-13 22:43:36 +0530

    hollow star

---
+++ star.java
@@ -0,0 +1,19 @@
+public class star {
+
+    public static void hollowstar(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i==1 || j==1 || i==n || j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int n=5;
+        hollowstar(n);
+    }
+}
