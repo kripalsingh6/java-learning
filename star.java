f7d9a980a9bbc86fca39bd018ef127bdfa3415b6
kripal <thakurkripalsingh6@gmail.com>
2026-01-26 17:53:44 +0530

    two sum

--- star.java
+++ star.java
@@ -221,10 +221,23 @@ public static void doublehollowrom(int n){
        
         
     }
+     public static int[] twoSum(int[] nums, int target) {
+
+         for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+
+
+    }
 
     public static void main(String args[]){
         int nums[]={4,2,1,3};
-        int k=2;
+        int target=5;
         star obj = new star(); 
         // hollowstar(n);
         // triNum(n);
@@ -236,6 +249,7 @@ public static void doublehollowrom(int n){
         // square(n);
     //   rhombus(n);
     // doublehollowrom(n);
-    System.out.println(obj.minimumAbsDifference(nums));
+    // System.out.println(obj.minimumAbsDifference(nums));
+    System.out.println(twoSum(nums,target));
     }
 }
