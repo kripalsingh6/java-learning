f674100bb23b92107d85f25c8a47225f9193675b
kripal <thakurkripalsingh6@gmail.com>
2026-02-08 13:20:27 +0530

    trees

---
+++ binaryTree.java
@@ -0,0 +1,43 @@
+import org.w3c.dom.Node;
+
+public class binaryTree {
+    static class Node {
+      int data;
+      Node left;
+      Node right;
+      
+       Node (int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+      }
+        
+    }
+    static class InnerbinaryTree {
+         static int idx=-1;
+         public static Node Buildtree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode= new Node(nodes[idx]);
+            newNode.left = Buildtree(nodes);
+            newNode.right= Buildtree(nodes);
+             return newNode;
+         }
+        
+    }
+    public static void preorder(Node root){
+        if(root==null){
+            return ;
+        }
+        System.out.println(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String[] args){
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+       Node root = InnerbinaryTree.Buildtree(nodes);
+        preorder(root);
+    }
+}
