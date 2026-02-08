46881fa432298906fdf4e036e51f0deaa337eb2c
kripal <thakurkripalsingh6@gmail.com>
2026-02-08 13:47:11 +0530

    level order

--- binaryTree.java
+++ binaryTree.java
@@ -1,3 +1,6 @@
+import java.util.Queue;
+import java.util.*;
+
 import org.w3c.dom.Node;
 
 public class binaryTree {
@@ -13,6 +16,7 @@ public class binaryTree {
       }
         
     }
+
     static class InnerbinaryTree {
          static int idx=-1;
          public static Node Buildtree(int nodes[]){
@@ -35,9 +39,39 @@ public class binaryTree {
         preorder(root.left);
         preorder(root.right);
     }
+    public static void levelOrder(Node root){
+        if(root==null){
+            return;
+        }
+        Queue<Node> q= new LinkedList();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+            }else{
+                q.add(null);
+            }
+        }
+        else{
+            System.out.println(currNode.data);
+            if(currNode.left!=null){
+                q.add(currNode.left);
+            }
+            if(currNode.right !=null){
+                q.add(currNode.right);
+            }
+        }
+        
+    }
+}
     public static void main(String[] args){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
        Node root = InnerbinaryTree.Buildtree(nodes);
-        preorder(root);
+        // preorder(root);
+        levelOrder(root);
     }
 }
