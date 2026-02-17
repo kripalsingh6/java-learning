f79a744f192d14249836fe94216d42597a787881
kripal <thakurkripalsingh6@gmail.com>
2026-02-17 23:25:13 +0530

    read bit

--- binaryTree.java
+++ binaryTree.java
@@ -192,9 +192,25 @@ public class binaryTree {
         }
         return result;
     }
+      public static List<String> readBinaryWatch(int turnedOn) {
+        List<String> result= new ArrayList<>();
+
+        for(int hour=0; hour<12; hour++){
+            for(int minute=0; minute<60; minute++){
+
+                int totalcount= Integer.bitCount(hour)+ Integer.bitCount(minute);
+                if(totalcount==turnedOn){
+                    result.add(hour + ":" + String.format("%02d",minute));
+                }
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
-         int nums[]={2,4,2,4,3,3,5};
-         System.out.println(singleNumber(nums));
+        //  int nums[]={2,4,2,4,3,3,5};
+        //  System.out.println(singleNumber(nums));
+        int n=1;
+        System.out.println(readBinaryWatch(n));
         // System.out.println(longestBalanced(nums));
         //  subarray(nums);
         // String a= "10101";
