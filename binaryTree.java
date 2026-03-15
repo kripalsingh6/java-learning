fe5331b5bdb0007a0adffc55253088a04e101c50
kripal <thakurkripalsingh6@gmail.com>
2026-03-15 18:10:17 +0530

    lowest common parent node

--- binaryTree.java
+++ binaryTree.java
@@ -262,13 +262,52 @@ public class binaryTree {
         int fact=n*value(n-1);
         return fact;
     }
+    public static boolean getpath(Node root, int n , ArrayList<Node> path){
+
+        if(root==null){
+            return false;
+        }
+        path.add(root);
+        if(root.data == n ){
+            return true;
+        }
+
+        boolean foundleft= getpath(root.left, n, path);
+        boolean foundright= getpath(root.right, n, path);
+
+        if(foundleft || foundright){
+            return true;
+        }
+          path.remove(path.size()-1);
+        return false;
+        
+    }
+
+    public static Node lca(Node root , int n1 , int n2){
+
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2= new ArrayList<>();
+
+        getpath(root, n1 , path1);
+        getpath(root, n2, path2);
+        int i=0;
+
+        for(; i<path1.size() && i<path2.size(); i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+        Node lca = path1.get(i-1);
+        return lca;
+        
+    }
     public static void main(String[] args) {
         //  int nums[]={2,4,2,4,3,3,5};
         //  System.out.println(singleNumber(nums));
         // String n="00110010";
         // System.out.println(countBinarySubstrings(n));
-        int n=5;
-        System.out.println(value(n));
+        // int n=5;
+        // System.out.println(value(n));
         // System.out.println(longestBalanced(nums));
         //  subarray(nums);
         // String a= "10101";
@@ -277,10 +316,21 @@ public class binaryTree {
         // System.out.println(reverseBits(52135));
 
 
-        // Node root = new Node(1);
-        // root.right = new Node(2);
-        // root.right.right = new Node(3);
-        // root.right.right.right = new Node(4);
+         Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+       root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+       root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1=4;
+        int n2=5;
+
+        System.out.println(lca(root, n1, n2).data);
 
         // Node balancedRoot = BalancedSearch(root);
         // inorder(balancedRoot); // test
